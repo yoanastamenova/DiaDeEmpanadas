eb6282b410d66ce8f7297a09e33afc3bb840dbf7
Yoana <yoanabarbie@icloud.com>
2024-06-13 19:54:49 +0200

    feat: added first parameters and some conditions

--- index.js
+++
@@ -1,7 +0,0 @@
-let primerTipo;
-let segundoTipo;
-let tercerTipo;
-
-function tresPorUno(primerTipo,segundoTipo,tercerTipo){
-    
-}
--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-export const fn = (/* argumentos de la función */) => {
-   // Cuerpo de la función: Completa la lógica de la función aquí.
+export const fn = (a, b, c) => {
+   let conjunto = a + b + c;
+   let tipoA;
+   let tipoB;
+   let tipoC;
+   let precioFinal;
+
+   if(a < 0 || b < 0 || c < 0){
+      console.log("Enter valid number of empanadas");
+   }
+   
+   if(a + b + c > 40){
+      console.log("The total of empanadas surpasses the max amount!");
+   }
+
+   if(a + b + c % 3 !== 0){
+      console.log("The total could not be divided by 3!");
+   }
+
+
+
+
 };
 
-zzzz
+
+
 
