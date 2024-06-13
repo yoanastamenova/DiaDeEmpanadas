fd59e19826136ea6b51f82b53943bf1d9195ebf7
Yoana <yoanabarbie@icloud.com>
2024-06-13 23:01:01 +0200

    feat: started the function in parts

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 export const fn = (a, b, c) => {
    let conjunto = a + b + c;
-   let tipoA;
-   let tipoB;
-   let tipoC;
+   let tipoA = 12;
+   let tipoB = 14;
+   let tipoC = 16;
    let precioFinal;
 
    if(a < 0 || b < 0 || c < 0){
@@ -17,8 +17,20 @@ export const fn = (a, b, c) => {
       console.log("The total could not be divided by 3!");
    }
 
+   /*we always need to have 1 of each since the promo is 3x1 
+   as well we need to identify the most and the least expensive prices in order to divide the total cost*/
+  t
+   if(tipoA == 1 && tipoB == 1 + tipoC == 1){
+      console.log((tipoA+tipoB+tipoC)/3 == 14);
+   }
 
+   if(tipoA == 2 && tipoB == 0 && tipoC ==1){
+      console.log((tipoA+tipoB+tipoC)/3 == 14)
+   }
 
+   if(tipoA == 3 && tipoB == 3 && tipoC ==0){
+      console.log((tipoA+tipoB+tipoC)/3 == 26)
+   }
 
 };
 
