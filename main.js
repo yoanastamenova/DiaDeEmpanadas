db6ee6078f49c59ad94ab085f9e97cd722f621ca
Yoana <yoanabarbie@icloud.com>
2024-06-16 20:13:16 +0200

    fix: changed the comments of the code a bit

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 export const fn = (a, b, c) => {
   //Declaring the variables and checking if they are in the correct range
   const total = a + b + c;
+  //creating new variable for the total count of our empanadas
+
   if (Math.min(a, b, c) < 0) throw new Error("The input value cannot be negative");
   if (total % 3 !== 0) throw new Error("The total sum of inputs has to be a multiple of 3");
   if (total >= 40) throw new Error("The total sum of the inputs has to be less than 40");
 
-  //Creating an array for the empanadas each of their own type, fills the arrays with prices and then makes them in a single array
+  //New array of empanada prices. It creates three separate arrays for each type of empanada, fills them with the their prices, and then concatenates them into a single array.
   let empanadas = Array(a).fill(12).concat(Array(b).fill(14), Array(c).fill(16));
   
-  //Here we have the calculation of the combined prices 
+  //Here we have an array for the calculation of the combined prices 
   let comPrice = [];
 
-  while (empanadas.length > 1) {   //taking the last pieces of the empanadas, calculating their average n pushing it twice into combPrices array
+  while (empanadas.length > 1) {   //Calculating the average prices as taking the first and last prices from the emp array, calculate the average (sum/count) and then push it twice into the combined prices array
     comPrice.push(...Array(2).fill((empanadas.shift() + empanadas.pop()) / 2));
-  }
+  }    //this is repeated until there is only one price left in the empanadas array
   if (empanadas.length === 1) comPrice.push(empanadas.pop()); //if one price is left in the empanadas then it is pushed to combPrices
 
   //sorts the prices from higher to lower
@@ -24,4 +26,4 @@ export const fn = (a, b, c) => {
 
   //lastly we return the total price by adding up all the pieces in the finalPrice array
   return finalPrice.reduce((total, price) => total + price, 0);
-};
+};  
